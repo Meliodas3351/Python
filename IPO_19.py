8d6dc24380fbd2946c076c709f286015ebc52724
Alex-Breywo <alex.breywo.3351@gmail.com>
2022-05-19 18:37:39 +0300

    Python#master: added 19-21 tasks

--- IPO_19.py
+++ IPO_19.py
@@ -2,19 +2,35 @@ from wand.image import Image
 from wand.display import display
 
 
-with Image(filename="D:/MouseWebSite/images/mouse_1.jpg") as img1:
-    print('width =', img1.width)
-    print('height =', img1.height)
-    img = img1
-    display(img)
-    img.wave(amplitude=img.height / 50,
-             wave_length=img.width / 80)
-    img.save(filename="fx-wave.jpg")
-    img = img1
-    display(img)
-    img.vignette(sigma=3, x=10, y=10)
+with Image(filename="D:/MouseWebSite/images/mouse_1.jpg") as img:
+    # img.wave(amplitude=img.height / 50,       #волна
+    #          wave_length=img.width / 80)
+    # img.save(filename="fx-wave.jpg")
+
+    img.vignette(sigma=20, x=50, y=25)     #виньетка
     img.save(filename="fx-vignette.jpg")
-    img = img1
+
+    # img.shade(gray=True,      #оттенок
+    #           azimuth=286.0,
+    #           elevation=45.0)
+    # img.save(filename="effect-shade.jpg")
+
+    # img.swirl(degree=-90)     #вихрь
+    # img.save(filename='fx-swirl.jpg')
+
+    # with Image(filename="fx-wave.jpg") as left_eye:   #стереограмма
+    #     with Image(filename="fx-vignette.jpg") as right_eye:
+    #         with Image.stereogram(left=left_eye,
+    #                               right=right_eye) as img:
+    #             img.save(filename="fx-stereogram.jpg")
+    #             display(img)
+
+    # img.transform_colorspace("gray")  #эскиз
+    # img.sketch(0.5, 0.0, 98.0)
+    # img.save(filename="fx-sketch.jpg")
+
+    # img.charcoal(radius=1.5, sigma=0.5) #уголь
+    # img.save(filename="fx-charcoal.jpg")
 
     display(img)
 
---
+++ IPO_20-21.py
@@ -0,0 +1,31 @@
+import requests
+
+s_city = "Lida,BY"
+city_id = 1
+
+appid = "d3222b950dc6fea8dbe97cce27217e81"
+try:
+    res = requests.get("http://api.openweathermap.org/data/2.5/find",
+                 params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
+    data = res.json()
+    # получаем пакет в формате JSON.
+    # Разбираем пакет и получаем нужные значения по названиям полей
+    cities = ["{} ({})".format(d['name'], d['sys']['country'])
+              for d in data['list']]
+    print("city:", cities)
+    city_id = data['list'][0]['id']
+    print('city_id=', city_id)
+except Exception as e:
+    print("Exception (find):", e)
+    pass
+try:
+    res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                       params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+    data = res.json()
+    print(data)
+    for i in data['list']:
+        print(i['dt_txt'], '{0:+5.0f}'.format(i['main']['temp']), i['weather'][0]['description'])
+        #Часть до точки определяет минимальную ширину,Часть после точки урезает десятичную часть
+except Exception as e:
+    #print("Exception (forecast):", e)
+    pass
